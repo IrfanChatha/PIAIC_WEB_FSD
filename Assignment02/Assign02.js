8587e80944a6e8c037b0fd14018d4bb30070bb23
IrfanChatha <imchatha@hotmail.com>
2023-08-25 19:29:58 +0500

    Assignment 02 Updated

---
+++ Assignment02/Assign02.js
@@ -0,0 +1,30 @@
+"use strict";
+// - Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT."
+console.log("Hey Abu Hurairah, I have started learning TYPESCRIPT.");
+// - Store your name in a variable and print it.
+var myName = "My name is Irfan.";
+console.log(myName);
+// - Store 10 numbers in different variables
+//   - Add all of them and print the SUM
+//   - Print the Difference (subtraction)
+//   - Print the result after multiplying all
+var numOne = 1;
+var numTwo = 2;
+var numThree = 3;
+var numFour = 4;
+var numFive = 5;
+var numSix = 6;
+var numSeven = 7;
+var numEight = 8;
+var numNine = 9;
+var numTen = 10;
+var SUM = numOne + numTwo + numThree + numFour + numFive + numSix + numSeven + numEight + numNine + numTen;
+console.log(SUM);
+var DIFF = numOne - numTwo - numThree - numFour - numFive - numSix - numSeven - numEight - numNine - numTen;
+console.log(DIFF);
+// - Take two numbers and print the division result.
+var DIVIDE = numTen / numFive;
+console.log(DIVIDE);
+// - Now, perform all four operations with the given numbers, print the results, and observe the order in which the operations take place.
+var Results = numTwo + numEight - numOne * numThree / numFour;
+console.log(Results);
