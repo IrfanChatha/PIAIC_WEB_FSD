a948e4e372e18d265f0a0d51c98cab311aac333e
IrfanChatha <imchatha@hotmail.com>
2023-11-25 15:49:21 +0500

    Next JS Assignment 01

--- Quarter_Two_NextJS/NextJS First App/nextjs-firstapp/app/page.jsx
+++ Quarter_Two_NextJS/NextJS First App/nextjs-firstapp/app/page.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return ( 
     <div>
-    <h1 className="text-3xl font-bold bg-blue-700 text-center">Hello World</h1>
+    <h1 className="text-3xl font-bold bg-blue-700 text-center">Hello World!</h1>
   </div>
   )
 }
