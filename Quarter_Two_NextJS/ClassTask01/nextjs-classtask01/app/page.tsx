e8263099d1df13d885c732b00be6ad43083f72b4
IrfanChatha <imchatha@hotmail.com>
2023-11-26 09:53:07 +0500

    Assignment Next

--- Quarter_Two_NextJS/ClassTask01/nextjs-classtask01/app/page.tsx
+++ Quarter_Two_NextJS/ClassTask01/nextjs-classtask01/app/page.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import LandingPage from "./pages/LandingPage/page";
-import { Metadata } from 'next'
 
-export const metadata: Metadata = {
-  title: 'Irfan -- Xplore Skill',
-}
 export default function Home() {
   return (
     <>
