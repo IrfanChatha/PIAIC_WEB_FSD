a9cc8952e5d132d9a1e089c8fd8b897254e8eec7
IrfanChatha <imchatha@hotmail.com>
2023-12-06 17:01:56 +0500

    Next JS Assignment 02

---
+++ Quarter_Two_NextJS/NextJS Second App Assignment/nextjs-secondapp/src/app/contact/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function layout({ children }) {
+    return (
+      <html lang="en">
+        <body>
+        <h2>HEADER FROM CONTACT</h2>
+          {children}</body>
+      </html>
+    )
+  }
