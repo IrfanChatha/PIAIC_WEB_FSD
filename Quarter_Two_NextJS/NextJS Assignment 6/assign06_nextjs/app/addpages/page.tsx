1f4e956cf8979e930f584c7cf4adce91a91079f1
IrfanChatha <imchatha@hotmail.com>
2024-02-18 14:54:25 +0500

    Assignment 6

---
+++ Quarter_Two_NextJS/NextJS Assignment 6/assign06_nextjs/app/addpages/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react';
+
+const Addpage = () => {
+    return (
+        <div>
+            <h1>USE STATE</h1>
+        </div>
+    );
+}
+
+export default Addpage;
