95d1ff63cc9d0833c3882bb7db8c1eb3ab8d575a
IrfanChatha <imchatha@hotmail.com>
2024-02-20 19:14:06 +0500

    Assignment 6

--- Quarter_Two_NextJS/NextJS Assignment 6/assign06_nextjs/app/addpages/page.tsx
+++ Quarter_Two_NextJS/NextJS Assignment 6/assign06_nextjs/app/addpages/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Addpage = () => {
     return (
         <div>
-            <h1>USE STATE 2024</h1>
+            <h1>USE STATE PROGRAM 2024</h1>
         </div>
     );
 }
