19e5bca4c02411f0e37cf2575872bd18333234ff
IrfanChatha <imchatha@hotmail.com>
2024-02-21 22:19:58 +0500

    Assignment 6

--- Quarter_Two_NextJS/NextJS Assignment 6/assign06_nextjs/app/addpages/page.tsx
+++ Quarter_Two_NextJS/NextJS Assignment 6/assign06_nextjs/app/addpages/page.tsx
@@ -7,12 +7,17 @@ function Addpage() {
 
     const [title, setTitle] = useState("Old Title");
 
+    function newTitleHandler() { return setTitle("New Title") }
+
+    function oldTitleHandler() { return setTitle("Old Title") }
+
     return (
         <div>
             <h1><b>USE STATE PROGRAM 2024 {title}</b></h1>
 
+            <button onClick={newTitleHandler}>New Title</button>
 
-
+            <button onClick={oldTitleHandler}>Old Title</button>
         </div>
     );
 }
